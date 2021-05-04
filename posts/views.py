632c1ae99f13b3e6ea72ae1fc79a7cf4907677a4
Matt Walter <mwalter157@gmail.com>
2021-05-03 17:13:22 -0700

    Initial commit

---
+++ posts/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Posts
+
+class HomePageView(ListView):
+    model = Posts
+    template_name = "home.html"
+    context_object_name = "all_posts_list"
+
