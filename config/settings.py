f5a24e054aecc5b0e57f13f558bc8334db0d1aa3
Matt Walter <mwalter157@gmail.com>
2021-05-03 18:30:45 -0700

    Added text cases and requirements

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*0v_lj5jdt!%jg*f5b-wr2w0-8n!q5=n88!0ow-czmahqzm0v$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
